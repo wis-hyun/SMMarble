e4b9a8d13710afcaf1ced0cd466cfe6567410a80
김성현 <khyun8576@naver.com>
2023-12-03 22:08:30 +0900

    12.03

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #define FOODFILEPATH "marbleFoodConfig.txt"
 #define FESTFILEPATH "marbleFestivalConfig.txt"
 
-#define MAX_NODE    100
+
 
 //board configuration parameters
 static int board_nr;
@@ -23,10 +23,20 @@ static int food_nr;
 static int festival_nr;
 static int player_nr;
 
+typedef struct player{
+	int energy;
+	int position;
+	char name[MAX_CHARNAME];
+	int accumCredit;
+	int flag_graduate;
+}player_t;
+
+static player_t cur_player[MAX_PLAYER];
+#if 0
 static int player_energy[MAX_PLAYER];
 static int player_position[MAX_PLAYER];
 static char player_name [MAX_PLAYER][MAX_CHARGNAME];
-
+#endif
 
 //function prototypes
 #if 0
@@ -41,23 +51,31 @@ void* findGrade(int player, char *lectureName); //find the grade from the player
 void printGrades(int player); //print all the grade history of the player
 #endif
 
+
 void generatePlayers(int n, int initEnergy){
 	int i;
 	//n time loop
 	for(i=0; i<n; i++){
 		//input name
-		printf(); //안내문구 
-		scanf("%s", player_name[i]);
+		printf("input player %i's name :",i); //안내문구 
+		scanf("%s", cur_player[i].name);
 		fflush(stdin);
 		//set position
-		player_position[i]=0;
+		cur_player[i].position=0;
 		
 		//set energy
-		player_energy[i]=initEnergy; 
+		cur_player[i].energy=initEnergy; 
+		cur_player[i].accumCredit=0;
+		cur_player[i].flag_graduate=0;
 	} 
 }
 
-
+void printPlayerStatus(void){
+	int i;
+	for (i=0;i<player_nr;i++){
+		printf("%s : credit %i, energy %i, position %i\n", cur_player[i].name, cur_player[i].accumCredit, cur_player[i].energy, cur_player[i].position);	
+	}
+}
 
 int rolldie(int player)
 {
@@ -74,19 +92,31 @@ int rolldie(int player)
     return (rand()%MAX_DIE + 1);
 }
 
-#if 0
+
 //action code when a player stays at a node
 void actionNode(int player)
 {
+	int type = smmObj_getNodeType(cur_player[player].position);
     switch(type)
     {
         //case lecture:
+        case SMMNODE_TYPE_LECTURE:
+        	cur_player[player].accumCredit += smmObj_getNodeCredit(cur_player[player].position);
+			cur_player[player].energy -= smmObj_getNodeEnergy(cur_player[player].position);
+			
+			break;
+			
+		
         default:
             break;
     }
 }
-#endif
 
+void goForward(int player, int step)
+{
+	cur_player[player].position += step;
+	printf("%s go to node %i (name : %s)\n", cur_player[player].name, cur_player[player].position, smmObj_getNodeName(cur_player[player].position));
+}
 
 
 int main(int argc, const char * argv[]) {
@@ -97,6 +127,8 @@ int main(int argc, const char * argv[]) {
     int credit;
     int energy;
     int i;
+    int initEnergy;
+    int turn=0;
     
     board_nr = 0;
     food_nr = 0;
@@ -119,16 +151,21 @@ int main(int argc, const char * argv[]) {
     {
         //store the parameter set
         smmObj_genNode(name, type, credit, energy);
+        if (type==SMMNODE_TYPE_HOME)
+        	initEnergy = energy;
         board_nr++;
     }
     fclose(fp);
     printf("Total number of board nodes : %i\n", board_nr);
     
     for (i = 0;i<board_nr;i++)
-        printf("node %i : %s, %i, %i, %i(%s)\n", i, smmObj_getNodeName(i), smmObj_getNodeType(i), smmObj_getNodeCredit(i), smmObj_getNodeEnergy(i), smmObj_getTypeName(smmObj_getNodeType(i)));
+        printf("node %i : %s, %i(%s), credit %i, energy %i\n", i, smmObj_getNodeName(i), 
+                     smmObj_getNodeType(i), smmObj_getTypeName(smmObj_getNodeType(i)),
+                     smmObj_getNodeCredit(i), smmObj_getNodeEnergy(i));
     
     printf("(%s)", smmObj_getTypeName(SMMNODE_TYPE_LECTURE));
     
+	#if 0
     //2. food card config   
     if ((fp = fopen(FOODFILEPATH,"r")) == NULL)
     {
@@ -151,7 +188,7 @@ int main(int argc, const char * argv[]) {
     	printf("node %i: %s, %i, ", i, smmObj_getNodeName(i + board_nr), smmObj_getNodeEnergy(i + board_nr));
 	}
     
-    #if 0
+   
     //3. festival card config 
     if ((fp = fopen(FESTFILEPATH,"r")) == NULL)
     {
@@ -166,7 +203,7 @@ int main(int argc, const char * argv[]) {
     }
     fclose(fp);
     printf("Total number of festival cards : %i\n", festival_nr);
-    
+    #endif
     
     
     //2. Player configuration ---------------------------------------------------------------------------------
@@ -175,37 +212,37 @@ int main(int argc, const char * argv[]) {
     {
         //input player number to player_nr
         printf("input player no.:");
-    	scanf("%d", %player_nr);
+    	scanf("%d", &player_nr);
     	fflush(stdin);
     }
-    while (player_nr <0 || player > MAX_PLAYER);
+    while (player_nr <0 || player_nr > MAX_PLAYER);
     
     generatePlayers(player_nr, initEnergy);
     
     
     
-    
+
     //3. SM Marble game starts ---------------------------------------------------------------------------------
-    while () //is anybody graduated?
+    while (1) //is anybody graduated?
     {
         int die_result;
         
         //4-1. initial printing
-        //printPlayerStatus();
+        printPlayerStatus();
         
         //4-2. die rolling (if not in experiment)
-        //rolldie(int player)
+        die_result= rolldie(turn);
         
         //4-3. go forward
-        //goForward();
+        goForward(turn, die_result);
 
 		//4-4. take action at the destination node of the board
-        //actionNode();
+        actionNode(turn);
         
         //4-5. next turn
-        
+        turn = (turn+1)%player_nr;
     }
-    #endif
+    
     system("PAUSE");
     return 0;
 }
--- smm_object.c
+++ smm_object.c
@@ -11,7 +11,7 @@
 
 #define MAX_NODETYPE    7
 #define MAX_GRADE       9
-#define MAX_PLAYER        100
+#define MAX_NODE      100
 
 static char smmNodeName[SMMNODE_TYPE_MAX][MAX_CHARNAME] = {
 	"강의",
@@ -21,57 +21,71 @@ static char smmNodeName[SMMNODE_TYPE_MAX][MAX_CHARNAME] = {
 	"실험실로 이동",
 	"음식찬스",
 	"축제시간"
-}
+};
 
 char* smmObj_getTypeName(int type){
 	return (char*)smmNodeName[type];
 } 
 
+typedef struct smm_Object
+{
+	char name[MAX_CHARNAME];
+	int type;
+	int credit;
+	int energy;
+} smmObject_t;
+
+static smmObject_t smm_node[MAX_NODE];
+static int smmObj_noNode =0;
+
+#if 0
 static char smmObj_name[MAX_NODE][MAX_CHARNAME];
 static int smmObj_type[MAX_NODE];
 static int smmObj_credit[MAX_NODE];
 static int smmObj_energy[MAX_NODE];
-static int smmObj_noNode=0;
-
-
+#endif
 
 //object generation
 void smmObj_genNode(char* name, int type, int credit, int energy)
 {
+	#if 0
     strcpy(smmObj_name[smmObj_noNode], name);
     smmObj_type[smmObj_noNode] = type;
     smmObj_credit[smmObj_noNode] = credit;
     smmObj_energy[smmObj_noNode] = energy;
+    #endif
+    strcpy(smm_node[smmObj_noNode].name, name);
+    smm_node[smmObj_noNode].type = type;
+    smm_node[smmObj_noNode].credit = credit;
+    smm_node[smmObj_noNode].energy = energy;
     
     smmObj_noNode++;
 }
 
 char* smmObj_getNodeName(int node_nr)
 {
-    return strdup(smmObj_name[node_nr]);
+    return strdup(smm_node[node_nr].name);
 }
 
 int smmObj_getNodeType(int node_nr)
 {
-    return smmObj_type[node_nr];
+    return smm_node[node_nr].type;
 }
 
 int smmObj_getNodeCredit(int node_nr)
 {
-    return smmObj_credit[node_nr];
+    return smm_node[node_nr].credit;
 }
 
 int smmObj_getNodeEnergy(int node_nr)
 {
-    return smmObj_energy[node_nr];
+    return smm_node[node_nr].energy;
 }
 
+
+
 #if 0
-
-
 //member retrieving
-
-
 
 //element to string
 char* smmObj_getNodeName(smmNode_e type)
@@ -83,6 +97,5 @@ char* smmObj_getGradeName(smmGrade_e grade)
 {
     return smmGradeName[grade];
 }
-
 #endif
 
